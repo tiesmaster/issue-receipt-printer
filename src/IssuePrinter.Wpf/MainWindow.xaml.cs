2b0f2d0a5edba0df2830f1b8ccd68b34dc5133fa
Thijs Brobbel <t.brobbel@coolblue.nl>
2016-07-18 17:33:15 +0200

    Add showing of installed printers.

--- src/IssuePrinter.Wpf/MainWindow.xaml.cs
+++ src/IssuePrinter.Wpf/MainWindow.xaml.cs
@@ -1,10 +1,23 @@
-﻿namespace IssuePrinter.Wpf
+﻿using System.Drawing.Printing;
+using System.Linq;
+
+namespace IssuePrinter.Wpf
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            ConfigurePrinterInputBox();
+        }
+
+        private void ConfigurePrinterInputBox()
+        {
+            foreach(string printerName in PrinterSettings.InstalledPrinters)
+            {
+                PrinterInputBox.Items.Add(printerName);
+            }
+            PrinterInputBox.SelectedItem = PrinterInputBox.Items.OfType<string>().Last();
         }
     }
 }
