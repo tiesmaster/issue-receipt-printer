a7f9ee8be4bd714ef59d27aaec3082d2c41e9b67
Thijs Brobbel <thijsbrobbel@gmail.com>
2016-09-02 14:41:40 +0200

    Remove api prefix from route.

--- src/IssuePrinter.Web/App_Start/WebApiConfig.cs
+++ src/IssuePrinter.Web/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace IssuePrinter.Web
 {
@@ -9,14 +6,10 @@ namespace IssuePrinter.Web
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
